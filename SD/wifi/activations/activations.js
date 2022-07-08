84f5e33dea91415e80fa9fd00e2f2635898b77ee
ookke <ookke1@gmail.com>
2022-07-08 22:09:11 +0300

    refactored index.htm into a multi-tab container

---
+++ SD/wifi/activations/activations.js
@@ -0,0 +1,14 @@
+var fileController = {
+    attach: function() {
+       
+    },
+    getInitialMarkup: function() {
+        return ``;
+    },
+    destroy: function() {
+
+    },
+
+};
+
+dots.tabs.register('activations_tab', fileController);
---
+++ SD/wifi/tabs.js
@@ -0,0 +1,37 @@
+var dots = { };
+dots.tabs = { 
+    _registry: {},
+    _activeTabController: null,
+    register: (tabHeaderId, controller) => {
+        dots.tabs._registry[tabHeaderId] = controller;
+    },
+    init: () => {
+        let tabList = document.getElementById('dots_tab_list');
+        let tabs = Array.from(tabList.getElementsByTagName('li'));
+        tabs.forEach(tab => tab.addEventListener('click', (event) => {
+            let tabId = event.target.id;
+            dots.tabs.activateTab(tabId);
+        }));
+        let defaultTab = tabs.filter(tab => tab.className.indexOf('active') != -1)[0];
+        dots.tabs.activateTab(defaultTab.id);
+    },
+    activateTab: (id) => {
+        let container = document.getElementById('tabs_content');
+
+        //clean up old tab
+        if(dots.tabs._activeTabController != null) {
+            dots.tabs._activeTabController.destroy();
+        }
+        container.innerHtml = "";
+
+        //in with the new
+        let newTabController = dots.tabs._registry[id];
+        let initialHtml = newTabController.getInitialMarkup();
+        container.innerHTML = initialHtml;
+        newTabController.attach();
+        dots.tabs._activeTabController = newTabController;
+    }
+
+};
+
+
