043ddbf00268bc88ae3b6a62fab270edcfe749de
Nathan <nwdamgaard@gmail.com>
2022-07-08 22:25:34 -0700

    integrated the log viewer

---
+++ SD/wifi/log-viewer/graph.js
@@ -0,0 +1,233 @@
+
+var traces = [];
+var canvas;
+var image;
+var ctx;
+
+var isMouseDown = false;
+var initialMousePos = {};
+var mouseDelta = 0;
+var zoom = {};
+
+var colour = null;
+
+var colours = [
+    "#0000FF",
+    "#FF0000",
+    "#00FF00",
+    "#FFFF00"
+];
+
+init = function(graphCanvas) {
+    canvas = graphCanvas;
+    ctx = canvas.getContext("2d");
+    clearScreen();
+    image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+clearScreen = function() {
+    ctx.fillStyle = "#151515";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+resize = function(size) {
+    canvas.width = size;
+    canvas.height = size;
+    
+    if(traces.length == 0) {
+        clearScreen();
+        return;
+    }
+    graph();
+}
+
+const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
+
+min = function(array) {
+    let min = array[0];
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+max = function(array) {
+    let max = array[0];
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+// Converts a #ffffff hex string into an [r,g,b] array
+var h2r = function(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+    ] : null;
+};
+
+// Inverse of the above
+var r2h = function(rgb) {
+    return "#" + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
+};
+
+// Interpolates two [r,g,b] colors and returns an [r,g,b] of the result
+// Taken from the awesome ROT.js roguelike dev library at
+// https://github.com/ondras/rot.js
+var _interpolateColor = function(color1, color2, factor) {
+  if (arguments.length < 3) { factor = 0.5; }
+  var result = color1.slice();
+  for (var i=0;i<3;i++) {
+    result[i] = Math.round(result[i] + factor*(color2[i]-color1[i]));
+  }
+  return result;
+};
+
+// performs the above on two #ffffff hex strings
+var interpolateColour = function(color1, color2, factor) {
+    let rgb1 = h2r(color1);
+    let rgb2 = h2r(color2);
+    let result = _interpolateColor(rgb1, rgb2, factor);
+    return r2h(result);
+}
+
+graph = function() {
+    clearScreen();
+    if(traces.length == 0) return;
+    let traceHeight = canvas.height / traces.length;
+    traces.forEach((trace, traceIndex) => {
+        let y_min = min(trace.y);
+        let y_max = max(trace.y);
+        //console.log({x_min, x_max, y_min, y_max});
+        for(let i = 0; i < trace.x.length; i++) {
+            let x = map(trace.x[i], zoom.x_min, zoom.x_max, 0, canvas.width);
+            let y = map(trace.y[i], y_min, y_max, traceHeight, 0) + traceHeight * traceIndex;
+            
+            if(colour != null) {
+                let factor = map(colour.values[i], colour.redValue, colour.greenValue, 0, 1);
+                if(factor < 0) {
+                    factor = 0;
+                }
+                if(factor > 1) {
+                    factor = 1;
+                }
+                ctx.fillStyle = interpolateColour("#ff0000", "#00ff00", factor);
+            } else {
+                ctx.fillStyle = colours[traceIndex % colours.length];
+            }
+            //ctx.fillStyle = "#ff0000";
+            
+            fillCircle(Math.round(x), Math.round(y), 2);
+        }
+    });
+
+    image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+fillCircle = function(centerX, centerY, radius, colour) {
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    ctx.fill();
+}
+
+resetZoom = function() {
+    if(traces.length == 0) return;
+    zoom = {
+        x_min: min(traces[0].x),
+        x_max: max(traces[0].x)
+    };
+}
+
+redraw = function() {
+    clearScreen();
+    ctx.putImageData(image, 0, 0);
+}
+
+mouseDown = function(mousePos) {
+    isMouseDown = true;
+    initialMousePos = mousePos;
+}
+
+mouseUp = function(mousePos) {
+    isMouseDown = false;
+
+    if(initialMousePos.x == mousePos.x && initialMousePos.y == mousePos.y) {
+        resetZoom();
+    } else {
+        zoom = {
+            x_min: map(initialMousePos.x, 0, canvas.width, zoom.x_min, zoom.x_max),
+            x_max: map(mousePos.x, 0, canvas.width, zoom.x_min, zoom.x_max)
+        };
+        if(zoom.x_max < zoom.x_min) {
+            let temp = zoom.x_max;
+            zoom.x_max = zoom.x_min;
+            zoom.x_min = temp;
+        }
+    }
+    
+    graph();
+}
+
+mouseMove = function(mousePos) {
+    if(!isMouseDown) return;
+
+    redraw();
+    ctx.fillStyle = "#FFFFFF55";
+    ctx.fillRect(initialMousePos.x, 0, mousePos.x - initialMousePos.x, canvas.height);
+}
+
+onmessage = function(e) {
+    //console.log("graph.js: recieved message: " + e.data.purpose);
+    
+    // listen: I know this is bad, just pretend it doesn't exist and I'll fix it later
+
+    if(e.data.purpose === "init") {
+        init(e.data.canvas);
+        return;
+    }
+
+    if(e.data.purpose === "resize") {
+        resize(e.data.size);
+        return;
+    }
+
+    if(e.data.purpose === "graphing") {
+        traces = e.data.traces;
+        resetZoom();
+        graph();
+        return;
+    }
+
+    if(e.data.purpose === "clearScreen") {
+        clearScreen();
+        return;
+    }
+    
+    if(e.data.purpose === "mouseMove") {
+        mouseMove(e.data.mousePos);
+        return;
+    }
+
+    if(e.data.purpose === "mouseDown") {
+        mouseDown(e.data.mousePos);
+        return;
+    }
+
+    if(e.data.purpose === "mouseUp") {
+        mouseUp(e.data.mousePos);
+        return;
+    }
+
+    if(e.data.purpose === "colours") {
+        colour = e.data.enabled ? e.data : null;
+        graph();
+        return;
+    }
+}
---
+++ SD/wifi/log-viewer/log-viewer.js
@@ -0,0 +1,237 @@
+let graph = null;
+
+// -------- just for tab support -----------
+var viewerController = {
+    attach: function() {
+        setupFileReader();
+
+        // TODO: the yaxes variable survives when navigating off the tab
+        //       which means we can reload all the graph settings they had.
+        //       Do that.
+
+        // TODO: init the graph worker when the page is initially loaded
+        //       that way we don't have to be connected to Gauge.S to
+        //       properly click on the log viewer tab
+        graph = new Worker("/wifi/log-viewer/graph.js");
+
+        let canvas = document.getElementById("chart");
+        if (!('transferControlToOffscreen' in canvas)) {
+            throw new Error('webgl in worker unsupported');
+        }
+
+        var offscreen = canvas.transferControlToOffscreen();
+        graph.postMessage({ 
+            purpose: "init",
+            canvas: offscreen 
+        }, [offscreen]);
+
+        sizeUI();
+    },
+    getInitialMarkup: function() {
+        return `<label for="fileInput">Upload log:</label>
+        <input type="file" accept=".csv" id="fileInput"/>
+        <div>
+            <canvas id="chart" onmousedown="chartMouseDown(event)" onmouseup="chartMouseUp(event)" onmousemove="chartMouseMove(event)"></canvas>
+            <div id="settingsDiv">
+                <p> 
+                    <label for="xselect">X axis:</label> 
+                    <select id="xselect" onchange="generatePlot()"></select> 
+                </p>
+                <div id="yaxiscontainer"></div>
+                <button type="button" onclick="addyaxis()">Add Y axis</button>
+                <div>
+                    <label for="coloursCheckbox">Colours:</label>
+                    <input id="coloursCheckbox" type="checkbox" onchange="toggleColours()"/>
+                    <div id="hiddenColoursPanel">
+                        <div>
+                            <select id="colourSelect" onchange="updateColours()"></select>
+                        </div>
+                        <div>
+                            <label for="redValue">Value of red</label>
+                            <input id="redValue" type="number" onchange="updateColours()"/>
+                        </div>
+                        <div>
+                            <label for="greenValue">Value of green</label>
+                            <input id="greenValue" type="number" onchange="updateColours()"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    },
+    destroy: function() {
+
+    },
+
+};
+
+dots.tabs.register('log_viewer_tab', viewerController);
+// ----------------------
+
+let logData = {};
+let columns = [];
+let yaxes = [];
+
+toggleColours = function() {
+    let coloursToggle = document.getElementById("coloursCheckbox");
+    let coloursPanel = document.getElementById("hiddenColoursPanel");
+    if(coloursToggle.checked) {
+        coloursPanel.style.display = "block";
+    } else {
+        coloursPanel.style.display = "none";
+    }
+    updateColours();
+}
+
+updateColours = function() {
+    let colourToggle = document.getElementById("coloursCheckbox");
+    let colourSelect = document.getElementById("colourSelect");
+    let redValue = document.getElementById("redValue");
+    let greenValue = document.getElementById("greenValue");
+    graph.postMessage({
+        purpose: "colours",
+        enabled: colourToggle.checked,
+        values: logData[colourSelect.value],
+        redValue: Number(redValue.value),
+        greenValue: Number(greenValue.value)
+    });
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+        y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+    };
+}
+
+chartMouseDown = function(e) {
+    graph.postMessage({
+        purpose: "mouseDown",
+        mousePos: getMousePos(document.getElementById("chart"), e)
+    });
+}
+
+chartMouseUp = function(e) {
+    graph.postMessage({
+        purpose: "mouseUp",
+        mousePos: getMousePos(document.getElementById("chart"), e)
+    });
+}
+
+chartMouseMove = function(e) {
+    graph.postMessage({
+        purpose: "mouseMove",
+        mousePos: getMousePos(document.getElementById("chart"), e)
+    });
+}
+
+addyaxis = () => {
+    let axis = document.createElement("div");
+    axis.style.display = "block";
+
+    let removeButton = document.createElement("button");
+    removeButton.type = "button";
+    removeButton.textContent = "Delete";
+    removeButton.onclick = () => {
+        let index = yaxes.indexOf(axis);
+        document.getElementById("yaxiscontainer").removeChild(yaxes[index]);
+        yaxes.splice(index, 1);
+        generatePlot();
+    };
+
+    let axisSelect = document.createElement("select");
+    axisSelect.setAttribute("onchange", "generatePlot()");
+    axisSelect.className = "varSelect";
+
+    columns.forEach((column) => {
+            addOption(axisSelect, column);
+    })
+    axis.appendChild(axisSelect);
+    axis.appendChild(removeButton);
+    document.getElementById("yaxiscontainer").appendChild(axis);
+
+    yaxes.push(axis);
+    generatePlot();
+}
+
+function generatePlot() {
+    let xvar = document.getElementById("xselect").value;
+
+    let traces = [];
+    yaxes.forEach((axis) => {
+        traces.push({
+            x: logData[xvar],
+            y: logData[axis.querySelector(".varSelect").value],
+            mode: 'markers',
+            type: 'scatter'
+        });
+    });
+    if(traces.length == 0) {
+        graph.postMessage({
+            purpose: "clearScreen"
+        })
+        return;
+    }
+
+    graph.postMessage({
+        purpose: "graphing",
+        traces: traces
+    });                
+}
+
+addOption = (select, value) => {
+    let option = document.createElement("option");
+    option.setAttribute("value", value);
+    option.appendChild(document.createTextNode(value));
+    select.appendChild(option);
+}
+
+setupFileReader = () => {
+    let picker = document.getElementById("fileInput");
+
+    picker.onchange = () => {
+        yaxes = [];
+        let selected = picker.files[0];
+        let reader = new FileReader();
+        reader.addEventListener("loadend", () => {
+            let temp = reader.result.split("\r\n");
+            for(let i in temp) {
+                temp[i] = temp[i].split(",");
+            }
+
+            for(let i in temp[0]) {
+                logData[temp[0][i]] = [];
+                for (let j=1; j<temp.length; j++) {
+                    logData[temp[0][i]].push(Number(temp[j][i]));
+                }
+
+                addOption(document.getElementById("xselect"), temp[0][i]);
+                addOption(document.getElementById("colourSelect"), temp[0][i]);
+                yaxes.forEach((axisDiv) => {
+                    let select = axisDiv.querySelector(".varSelect");
+                    addOption(select, temp[0][i]);
+                });
+
+                columns.push(temp[0][i]);
+            }
+            logData.points_count = temp.length - 1;
+            generatePlot();
+        });
+        reader.readAsText(selected);
+    }
+}
+
+min = (a, b) => {
+    return a < b ? a : b;
+}
+
+sizeUI = () => {
+    let canvas = document.getElementById("chart");
+    let chartSize = min(window.innerWidth, window.innerHeight) - 200 - document.getElementById("logo").height;
+    graph.postMessage({
+        purpose: "resize",
+        size: chartSize
+    })
+}
+window.onresize = sizeUI;
