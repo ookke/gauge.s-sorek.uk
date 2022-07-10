2bedaaffd77bcff5ae58af78427a6f6100dad6be
ookke <ookke1@gmail.com>
2022-07-10 13:41:43 +0300

    Use Cache API to cache .csv GETs

--- SD/wifi/common.js
+++ SD/wifi/common.js
@@ -13,6 +13,10 @@ dots.ui = {
 
 dots.http = {
     get: (url, success, error) => { 
+        if(url.indexOf('.csv') != -1) {
+            return dots.http._getCached(url, success, error);
+        }
+
         let xhr = new XMLHttpRequest();
         xhr.onload = () => {
             success(xhr.responseText)
@@ -24,6 +28,7 @@ dots.http = {
                 alert('Get failed, status '+xhr.status+ ' response: '+xhr.responseText);
             }
         }
+        
         xhr.open("GET", url, true);
         xhr.send(null);
     },
@@ -41,6 +46,18 @@ dots.http = {
             }
         });
 
+    },
+    _getCached: (url, success, error) =>  { 
+        caches.open('dots.cache').then(cache => {
+            let request = new Request(url);
+            cache.match(request).then(cachedResponse => {
+                if(cachedResponse != null) {
+                    cachedResponse.text().then((text) => success(text));
+                } else {
+                    cache.add(request).then(response => response.text().then(text => success(text)));
+                }
+            });
+        });
     },
     post: (url, payloadString, success, error) => {
         //TODO
