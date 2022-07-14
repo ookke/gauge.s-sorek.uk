f8167b1d2c58b0fb1cd0237024199960252054d0
ookke <ookke1@gmail.com>
2022-07-14 20:06:38 +0300

    dev server: further cleanup

--- SD/wifi/dev-server.js
+++ SD/wifi/dev-server.js
@@ -6,21 +6,18 @@ app.http().io();
 app.use("/wifi", express.static(__dirname));
 app.use("/", express.static(__dirname));
 
-app.get('/api/params/:paramHeader', function(req, res) {
-	console.log('got param get for: '+req.params.paramHeader);
-	res.json(Math.random());
-});
-
-app.get('/parameters', function(req, res) {
-	res.json({ ecuparam: [{ h:"asd", v: Math.sin(Date.now()) }] } );
-});
-
 //very half assed directory listing, only works for the root dir atm
 app.get('/list',  function(req, res) {
 	let contents = fs.readdirSync(__dirname);
 	var listing = contents.map(itm => ({name: itm, type: itm.indexOf('.') != -1  ? 'file' : 'directory'}));
 	res.json(listing);
 });
+
+app.get('/parameters', function(req, res) {
+	res.json({ ecuparam: [{ h:"asd", v: Math.sin(Date.now()) }] } );
+});
+
+
 
 app.listen(1337);
 
