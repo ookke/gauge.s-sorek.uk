3026d1716b8f4e7bb1bf0744564c0f26eb4c2430
ookke <ookke1@gmail.com>
2022-07-14 20:02:36 +0300

    dev server: slightly improve directory listings
    shows actual directory contents, but only for root dir atm

--- SD/wifi/dev-server.js
+++ SD/wifi/dev-server.js
@@ -1,4 +1,5 @@
 var express = require('express.io');
+var fs = require('fs');
 var app = express()
 app.http().io();
 
@@ -23,9 +24,11 @@ app.get('/parameters', function(req, res) {
 	res.json({ ecuparam: [{ h:"asd", v: Math.sin(Date.now()) }] } );
 });
 
-
+//very half assed directory listing, only works for the root dir atm
 app.get('/list',  function(req, res) {
-	res.json([{name: 'index.htm', type: 'file'},{name: 'Gauge.S-9_08-30.csv', type: 'file'}]);
+	let contents = fs.readdirSync(__dirname);
+	var listing = contents.map(itm => ({name: itm, type: itm.indexOf('.') != -1  ? 'file' : 'directory'}));
+	res.json(listing);
 });
 
 app.listen(1337);
