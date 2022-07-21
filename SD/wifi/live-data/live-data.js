65624886306655428b3e9dafb887008c9c18b0e1
ookke <ookke1@gmail.com>
2022-07-21 21:31:33 +0300

    use an icon for delete widget button

--- SD/wifi/live-data/live-data.js
+++ SD/wifi/live-data/live-data.js
@@ -209,18 +209,12 @@ let initDragEvents = () => {
         }
     }
 
-    let deleteBtnDiv = document.createElement('div');
-    deleteBtnDiv.id = 'delete_widget_button';
-
-    let deleteWidgetBtn = document.createElement('button');
-    deleteWidgetBtn.innerHTML = 'Delete';
-    deleteBtnDiv.appendChild(deleteWidgetBtn);
-    deleteWidgetBtn.addEventListener('click', () => {
+    let deleteBtnDiv = createDeleteButton();
+    deleteBtnDiv.addEventListener('click', () => {
         if(selectedWidget != null) {
             removeWidget(selectedWidget, selectedDiv);
         }
     })
-
     dashboardContainer.appendChild(deleteBtnDiv);
     
 
@@ -402,4 +396,16 @@ let listAvailableParameters = () => {
     //TODO: actual implementation would load config.json and parse ecuparams defs
     return [{header: 'Engine Speed', unit: 'rpm'}, { header: 'Coolant Temperature', unit: 'C'}]
 }
+
+
+let createDeleteButton = () => {
+
+    let deleteWidgetBtn = document.createElement('button');
+    deleteWidgetBtn.id = 'delete_widget_button';
+    deleteWidgetBtn.innerHTML =  `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>`;
+
+    return deleteWidgetBtn;
+}
+
+
 })();
