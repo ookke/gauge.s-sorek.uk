8ec1afeba30ab1bb2b7309bbacbba924a93deaec
ookke <ookke1@gmail.com>
2022-08-07 23:15:13 +0300

    added button to edit widget settings afterwards

--- SD/wifi/live-data/live-data.js
+++ SD/wifi/live-data/live-data.js
@@ -156,6 +156,10 @@ let showWidgetSettingsDialog = (widget, save, cancel) => {
         //TODO: should support at least bool also
         input.name = property;
         input.id = property;
+        if(property.value != null) {
+            //TODO: handle select options also so param doesn't get reset on settings edit
+            input.value = property.value;
+        }
         form.appendChild(input); 
         fields[property] = input;       
     }
@@ -220,6 +224,25 @@ let addWidget = (type) => {
 
 };
 
+let createWidgetFromInfo = (widgetInfo) => {
+    let widget = createWidget(widgetInfo.type);
+    widget.id = widgetInfo.id;
+    widget.type = widgetInfo.type;
+    widget.settings = widgetInfo.settings;
+    widget.size = widgetInfo.size;
+    widget.position = widgetInfo.position;
+
+    idToWidgetMapping[widget.id] = widget;
+    dashboard.pages[0].widgets.push(widget);
+
+    let dashboardContainer = document.getElementById('dashboard_container');
+
+    let widgetContainer = createWidgetContainer(widget);
+    dashboardContainer.appendChild(widgetContainer);
+    widget.init.bind(widget, widgetContainer)();
+    return widget;
+}
+
 let createWidgetContainer = (widget) => {
     let widgetContainer = document.createElement('div');
     widgetContainer.className = 'dots-widget';
@@ -249,13 +272,27 @@ let initDragEvents = () => {
         }
     }
 
-    let deleteBtnDiv = createDeleteButton();
-    deleteBtnDiv.addEventListener('click', () => {
+    let widgetButtons = document.createElement('div');
+    widgetButtons.id = 'widget_buttons_toolbar';
+
+    let deleteBtn = createDeleteButton();
+    widgetButtons.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', () => {
         if(selectedWidget != null) {
             removeWidget(selectedWidget, selectedDiv);
         }
     })
-    dashboardContainer.appendChild(deleteBtnDiv);
+
+    let settingsBtn = createSettingsButton();
+    widgetButtons.appendChild(settingsBtn);
+    settingsBtn.addEventListener('click', () => {
+        if(selectedWidget != null) {
+            openSettings(selectedWidget, selectedDiv);
+        }
+    });
+
+    
+    dashboardContainer.appendChild(widgetButtons);
     
 
     let dragging = false;
@@ -327,7 +364,8 @@ let initDragEvents = () => {
         }
 
         
-    };
+    }
+
     dashboardContainer.addEventListener('mousedown', listener);
     dashboardContainer.addEventListener('touchstart', listener);
 
@@ -349,6 +387,18 @@ let initDragEvents = () => {
         //TODO: widget.destroy() or smt to clean up listeners etc
     }
 
+    let openSettings = (selectedWidget, selectedDiv) => {
+
+        showWidgetSettingsDialog(selectedWidget, (save) => { 
+            removeWidget(selectedWidget, selectedDiv);
+            let newWidget = createWidgetFromInfo(selectedWidget);
+        }, (cancel) => {
+
+        });
+
+      
+    }
+
 
     let selectWidget = (widget, widgetContainer) => {
         if(selectedDiv != null) {
@@ -364,12 +414,12 @@ let initDragEvents = () => {
             var col = parseInt(selectedDiv.style.gridColumn.split('/')[0]);
             var row = parseInt(selectedDiv.style.gridRow.split('/')[0]);
 
-            deleteBtnDiv.style.display = 'block';
-            deleteBtnDiv.style.gridColumn = col + ' / span 1'; 
-            deleteBtnDiv.style.gridRow = row + ' / span 1';
+            widgetButtons.style.display = 'inline-block';
+            widgetButtons.style.gridColumn = col + ' / span 1'; 
+            widgetButtons.style.gridRow = row + ' / span 1';
         }
         else {
-            deleteBtnDiv.style.display = 'none';
+            widgetButtons.style.display = 'none';
         }
     }
 
@@ -425,19 +475,7 @@ let loadDashboard = () => {
         name: page.name,
         active: page.active,
         widgets: page.widgets.map(widgetInfo => {
-            //TODO: should share more code with addWidget()
-            var widget = createWidget(widgetInfo.type);
-            widget.id = widgetInfo.id;
-            idToWidgetMapping[widget.id] = widget;
-            widget.position = widgetInfo.position;
-            widget.settings = widgetInfo.settings;
-            widget.size = widgetInfo.size;
-            
-            let widgetContainer = createWidgetContainer(widget);
-            dashboardContainer.appendChild(widgetContainer);
-            widget.init.bind(widget, widgetContainer)();
-
-            return widget;  
+            return createWidgetFromInfo(widgetInfo);
         })
     }});
     dashboard = restoredDashboard;
@@ -445,15 +483,20 @@ let loadDashboard = () => {
 
 
 
-
-
 let createDeleteButton = () => {
 
     let deleteWidgetBtn = document.createElement('button');
     deleteWidgetBtn.id = 'delete_widget_button';
-    deleteWidgetBtn.innerHTML =  `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>`;
+    deleteWidgetBtn.innerHTML =  `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="widget-button-icon" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>`;
 
     return deleteWidgetBtn;
+}
+
+let createSettingsButton = () => {
+    let settingsBtn = document.createElement('button');
+    settingsBtn.id = 'widget_settings_button';
+    settingsBtn.innerHTML = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg"  width="16" height="16" fill="currentColor" class="widget-button-icon" viewBox="0 0 512 512" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 512 512"><g><line y1="501" x1="303.1" y2="501" x2="302.1"/><g><path d="m501,300.8v-91.7h-45.3c-5.3-22.4-14.3-43.3-26.4-62.1l32.9-32.7-64.9-64.6-33.4,33.3c-18.8-11.5-39.6-19.9-61.8-24.8v-47.2h-92.1v48.3c-22,5.4-42.6,14.4-61.1,26.4l-34.2-34-64.9,64.6 35.3,35.2c-2.8,4.6-5.3,9.2-7.7,14-7.5,14.3-13.2,30-17.1,45.7h-49.3v91.7h50.3c1.5,6 3.3,11.9 5.3,17.8 0.1,0.4 0.3,0.8 0.4,1.2 0,0.1 0.1,0.2 0.1,0.4 4.9,14.2 11.3,27.7 19.1,40.2l-35.5,35.3 64.9,64.6 35.1-34.9c18.3,11.5 38.6,20.2 60.2,25.4v48.1h91.1v-47.1c22.7-5 44-13.7 63.1-25.6l32.2,32 64.9-64.6-32.1-31.9c12-19.1 20.9-40.3 26-62.9h44.9zm-94.8,64l29.9,29.8-36.6,36.5-29.5-29.4c-24.7,18.9-54.4,31.7-86.7,36v42.4h-51.3v-42.7c-31.5-4.6-60.4-17.2-84.6-35.7l-31.6,31.5-36.6-36.5 32.4-31.3c-17.9-24-30-52.4-34.4-83.4h-45.3v-51.1h44l1.5-3.6c4.7-29.7 16.5-57.1 33.6-80.3l-32-31.9 36.6-36.5 31,31.9c24-18.5 52.8-31.2 84.1-36v-42.7h51.3v42.3c32,4.1 61.3,16.4 86,34.8l30.3-30.1 35.6,36.5-29.6,29.5c18.2,23.8 30.7,52.2 35.5,83.1h45.4v51.1h-44.7c-3.9,31.8-16.1,61.1-34.3,85.8z"/><path d="m257,143.4c-61.8,0-113.1,50-113.1,112.6s51.4,112.6 113.1,112.6 113.1-51.1 113.1-112.6-51.3-112.6-113.1-112.6zm0,204.3c-51.3,0-92.1-40.7-92.1-91.7 0-51.1 41.9-91.7 92.1-91.7s92.1,40.7 92.1,91.7c0.1,51.1-41.8,91.7-92.1,91.7z"/></g></g></svg>`;
+    return settingsBtn;
 }
 
 
